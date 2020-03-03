aa94eb36745a2da50564a6ae7901172d47d17696
Thiago Valentim <thiago@criawebs.com.br>
2020-03-03 12:29:16 -0300

    improvement: interface improvements to turn the ux better

--- client/src/components/TableDeck/index.tsx
+++ client/src/components/TableDeck/index.tsx
@@ -3,6 +3,7 @@ import { TableCard } from "../TableCard";
 import { BookingApi } from "../../services/booking/BookingApi";
 import Container from "react-bootstrap/Container";
 import CardDeck from 'react-bootstrap/CardDeck';
+import Badge from 'react-bootstrap/Badge';
 
 type SliderState = {
     restaurants: any[]
@@ -26,9 +27,8 @@ export class TableDeck extends Component<{}, SliderState> {
         const bookingApi = new BookingApi('');
         bookingApi.listRestaurants().then(response => {
             response.json().then(data => {
-                const restaurantsData = data.data;
-                const filteredRestaurants = restaurantsData.filter((r: any) => r.tables.length > 0).sort((a: any, b:any) => b.tables.length - a.tables.length);
-                this.setState({ restaurants: filteredRestaurants });
+                const restaurantsData = data.data.sort((a: any, b:any) => b.tables.length - a.tables.length);
+                this.setState({ restaurants: restaurantsData });
             });
         });
 
@@ -39,13 +39,22 @@ export class TableDeck extends Component<{}, SliderState> {
         return(
             <Container>
                 {restaurants.map((restaurant: any) =>
-                    <CardDeck key={restaurant.guid} className="d-flex justify-content-center">
-                        {restaurant.tables.map((table: any) =>
-                            <div key={table.guid} >
-                                <TableCard restaurantName={restaurant.name} guid={table.guid} positionName={table.positionName} maxSeats={table.maxSeats} restaurantGuid={restaurant.guid} />
-                            </div>
-                        )}
-                    </CardDeck>
+                    <div key={restaurant.guid}>
+                        <h2><Badge variant="info" className="d-flex justify-content-center badge-restaurant">{restaurant.name}</Badge></h2>
+                        <CardDeck className="d-flex">
+                            {restaurant.tables.map((table: any) =>
+                                <div key={table.guid} >
+                                    <TableCard open={restaurant.opensAt} close={restaurant.closesAt} restaurantName={restaurant.name} available={true} guid={table.guid} positionName={table.positionName} maxSeats={table.maxSeats} restaurantGuid={restaurant.guid} />
+                                </div>
+                            )}
+
+                            {restaurant.tables.length < 1 && (
+                                <div>
+                                    <TableCard open={restaurant.opensAt} close={restaurant.closesAt} restaurantName={restaurant.name} available={false} guid={restaurant.guid} positionName={restaurant.name} maxSeats={0} restaurantGuid={restaurant.guid} />
+                                </div>
+                            )}
+                        </CardDeck>
+                    </div>
                 )}
             </Container>
         );
