ae147d19577473148d30e7202cb4bc16c715b963
Thiago Valentim <thiago@criawebs.com.br>
2020-03-03 22:20:55 -0300

    refactory: add some interface improvements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,55 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav'
 import { TableDeck } from './components/TableDeck';
 import { Manager } from './components/Pages/Manager';
-import './App.css';
 import { Reservation } from './components/Pages/Reservation';
+import { BookingApi } from './services/booking/BookingApi';
+import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-        <Navbar className="bg-light-blue" sticky="top" expand="lg">
-            <Navbar.Brand href="#restaurants">Menu</Navbar.Brand>
-            <Nav fill defaultActiveKey="/">
-                <Nav.Item>
-                    <Link to="/" className="nav-link">Restaurants</Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Link to="/manager" className="nav-link">Manager</Link>
-                </Nav.Item>
-            </Nav>
-        </Navbar>
-        <Container className="p-3">
-            <Switch>
-                <Route exact path="/" component={TableDeck} />
-                <Route exact path="/manager" component={Manager} />
-                <Route path="/restaurants/:restaurantGuid/tables/:tableGuid/add-reservation"
-                        render={props => {
-                            return <Reservation restaurantGuid={props.match.params.restaurantGuid} tableGuid={props.match.params.tableGuid} />;
-                        }}
-                />
-            </Switch>
-        </Container>
-    </BrowserRouter>
-  );
+
+    const bookingApi = new BookingApi();
+
+    return (
+        <BrowserRouter>
+            <Navbar className="bg-light-blue" sticky="top" expand="lg">
+                <Navbar.Brand href="#restaurants">Menu</Navbar.Brand>
+                <Nav fill defaultActiveKey="/">
+                    <Nav.Item>
+                        <Link to="/" className="nav-link">Restaurants</Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Link to="/manager" className="nav-link">Manager</Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar>
+            <Container className="p-3">
+                <Switch>
+                    <Route exact path="/" render={props => {
+                                return <TableDeck bookingApi={bookingApi} />;
+                            }}
+                    />
+                    <Route exact path="/manager" render={props => {
+                                return <Manager bookingApi={bookingApi} />;
+                            }}
+                    />
+                    <Route path="/restaurants/:restaurantGuid/tables/:tableGuid/add-reservation"
+                            render={props => {
+                                return <Reservation bookingApi={bookingApi} restaurantGuid={props.match.params.restaurantGuid} tableGuid={props.match.params.tableGuid} />;
+                            }}
+                    />
+                    <Route path="/restaurants/:restaurantGuid/tables/:tableGuid/waiting-queue"
+                            render={props => {
+                                return <Reservation bookingApi={bookingApi} isWaitingQueueReservation={true} restaurantGuid={props.match.params.restaurantGuid} tableGuid={props.match.params.tableGuid} />;
+                            }}
+                    />
+                </Switch>
+            </Container>
+        </BrowserRouter>
+    );
 };
 
 export default App;
